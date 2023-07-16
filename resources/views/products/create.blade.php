471bc10b653850e13826f3dac0150cfc7d9ad4da
Selahadin Jemal <sjemal@jsi.local>
2023-07-16 08:09:01 -0400

    Products view created

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,6 @@
+@extends('products.layout')
+
+@section('create')
+<h2>create Product</h2>
+
+@endsection
---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,16 @@
+<!-- Stored in resources/views/layouts/app.blade.php -->
+ 
+<html>
+<head>
+    <title>Laravel crud</title>
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <h1>Main Layout</h1>
+    <div class="container">
+        @yield('create')
+    </div>
+</body>
+</html>
