3d8af34772d24f8e8a4f618e8b8553c07fec2254
Selahadin Jemal <sjemal@jsi.local>
2023-07-20 10:58:18 +0300

    created models, changed db to access pipatzam_svc

--- app/Models/Site.php
+++ app/Models/Site.php
@@ -5,9 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Region extends Model
+class Site extends Model
 {
     use HasFactory;
-    protected $table = 'svc_regions';
-    public $timestamps = false;
 }
